16aebbd3db30d5ceea6f6c7c6920e4ff2112527b
Tim B. Ofenbach <t.ofenbach@web.de>
2018-05-19 18:31:08 +0200

    added refrain + better combinations

--- code/song_generator.py
+++ code/song_generator.py
@@ -1,35 +1,40 @@
 import sys
 import random
 
-chords = ['D', 'Dm', 'E','Em','A','Am','F','G']	# TODO add every basic chord
-
-def play_chord(id):
-	""" Doesn't work (yet)! """
- 	system.play(chords[id])
+# Chords
+d_chords = ['D', 'Dm']
+e_chord = ['E', 'Em']
+a_chords = ['A', 'Am']
+f_chords = ['F', 'Fm']
+g_chords = ['G', 'Gm']
+chords = [d_chords, e_chord, a_chords, f_chords, g_chords]
 
 def generate_chords(amount):
 	""" Generates basic chords.
 	    param: amount of chords (int) """
 	final_chords = []
 
-	for i in range(amount):	# TODO fix double chords AND not sounding good combinations (e.g. D, Dm):
-		final_chords.append(chords[random.randint(0,amount)])
+	for i in range(amount):	# TODO fix double chords
+		final_chords.append(chords[random.randint(0,4)][random.randint(0,1)])
 
 	return final_chords
 
 def main():
 
-	# setup
-	arg_list = sys.argv[1:]
-	intro_chord_list = []
-	bridge_chord_list = []
+  # setup
+  arg_list = sys.argv[1:]
+  intro_chord_list = []
+  bridge_chord_list = []
+  refrain_chord_list = []
 
-	# generate chords
-	intro_chord_list = generate_chords(4)	#TODO fix: if (arg_list[0] == "chords"):
-	bridge_chord_list = generate_chords(2)
+  # generate chords TODO: if (arg_list[0] == "-chords"):
+  intro_chord_list = generate_chords(4)
+  bridge_chord_list = generate_chords(2)
+  refrain_chord_list = generate_chords(4)
 
-	# display chords
-	print "Intro Chords: " + str(intro_chord_list) + "\n"
-	print "Bridge Chords " +  str(bridge_chord_list) + "\n"
+  # display chords
+  print("Intro Chords: " + str(intro_chord_list))
+  print("Bridge Chords: " + str(bridge_chord_list))
+  print("Refrain Chords: " + str(refrain_chord_list))
 
 main()
